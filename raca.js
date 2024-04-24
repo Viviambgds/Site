41881acea3f16e471a742beeaf4e57839ae4f625
Viviambgds <167723017+Viviambgds@users.noreply.github.com>
2024-04-24 02:53:05 -0300

    Delete raca.js

--- raca.js
+++
@@ -1,9 +0,0 @@
-function mostrarPopup(titulo, texto) {
-    document.getElementById("titulo-popup").textContent = titulo;
-    document.getElementById("texto-popup").textContent = texto;
-    document.getElementById("popup").style.display = "flex";
-}
-
-function fecharPopup() {
-    document.getElementById("popup").style.display = "none";
-}
